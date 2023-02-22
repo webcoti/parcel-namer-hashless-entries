1bfd84393a83868b51662e956486b5125bb1a267
Fabiano Parente <fabiano@nunes.me>
2023-02-22 10:02:41 -0300

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# @webcoti/parcel-namer-hashless-entries
+
+## Install
+
+```console
+npm install @webcoti/parcel-namer-hashless-entries --save-dev
+```
+
+## Usage
+
+Add a `.parcelrc` into your root directory:
+
+```json
+{
+  "extends": "@parcel/config-default",
+  "namers": ["@webcoti/parcel-namer-hashless-entries", "..."]
+}
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { Namer } = require('@parcel/plugin')
+const { default: defaultName } = require('@parcel/namer-default')
+
+const CONFIG = Symbol.for('parcel-plugin-config')
+
+function buildNameWithoutHash(bundle, oldName) {
+  if (bundle?.needsStableName) return oldName
+
+  const pieces = oldName.split('.')
+  pieces.splice(pieces.length - 2, 1)
+  return pieces.join('.')
+}
+
+module.exports = new Namer({
+  async name({ bundle, bundleGraph, logger }) {
+    const group = bundleGraph.getBundleGroupsContainingBundle(bundle)[0]
+    const oldName = await defaultName[CONFIG].name({
+      bundle,
+      bundleGraph,
+      logger,
+    })
+
+    return bundleGraph.isEntryBundleGroup(group)
+      ? buildNameWithoutHash(bundle, oldName)
+      : oldName
+  },
+})
